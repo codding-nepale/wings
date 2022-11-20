1a80a6195f0c217668d513da99fcf7861f30f46b
codding-nepale <79444135+codding-nepale@users.noreply.github.com>
2022-11-20 14:56:12 +0100

    Add files via upload

---
+++ wings.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/pterodactyl/wings/cmd"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Since we make use of the math/rand package in the code, especially for generating
+	// non-cryptographically secure random strings we need to seed the RNG. Just make use
+	// of the current time for this.
+	rand.Seed(time.Now().UnixNano())
+
+	// Execute the main binary code.
+	cmd.Execute()
+}
